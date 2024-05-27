c0f802e5a3c6f2d52d1f31658d7e923e10b35a5d
Martin hapl <martin@hapl.cz>
2024-05-27 20:42:45 +0200

    Latest updates

--- update.sh
+++ update.sh
@@ -81,6 +81,17 @@ function checkKlipper() {
     fi
 }
 
+function updateSpider() {
+    # Update Spider
+    echo "*** Updating Spider ***"
+
+    cp .config.spider .config
+    make clean
+    make
+    make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_4B0032000250563046353420-if00
+    pause "Spider updated, press ENTER to continue with Pico"
+}
+
 pause "Press ENTER to start the update"
 
 checkSudo
@@ -95,23 +106,18 @@ cd $KLIPPER_PATH
 echo "*** Stopping Klipper ***"
 sudo service klipper stop
 
-# Update Spider
-echo "*** Updating Spider ***"
-cp .config.spider .config
-make clean
-make
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_4B0032000250563046353420-if00
-pause "Spider updated, press ENTER to continue with Pico"
+# Update Spide
+updateSpider
 
 #Update Pico
-echo "*** Updating Pico ***"
-cp .config.pico .config
-make clean
-make
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_rp2040_E6609CB2D3243128-if00
+#echo "*** Updating Pico ***"
+#cp .config.pico .config
+#make clean
+#make
+#make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_rp2040_E6609CB2D3243128-if00
 
-echo "waiting for Pico to come up"
-while [ ! -e "/dev/serial/by-id/usb-Klipper_rp2040_E6609CB2D3243128-if00" ]; do sleep 0.1; done
+#echo "waiting for Pico to come up"
+#while [ ! -e "/dev/serial/by-id/usb-Klipper_rp2040_E6609CB2D3243128-if00" ]; do sleep 0.1; done
 
 echo "*** Starting Klipper again ***"
 sudo service klipper start
